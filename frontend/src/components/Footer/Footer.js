e31935172e8cc11b4ad928191e39274342c414df
Rahul Jain <ee1170476@iitd.ac.in>
2021-05-23 20:38:09 +0530

    Change Footer Details

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,75 +1,3 @@
-// import React from "react";
-// import {
-//   Box,
-//   Container,
-//   Row,
-//   Column,
-//   FooterLink,
-//   Heading,
-// } from "./FooterStyles";
-
-// const Footer = () => {
-//   return (
-//     <Box>
-//       <Container>
-//         <Row>
-//           {/* <Column>
-//             <Heading>About Us</Heading>
-//             <FooterLink href="#">Aim</FooterLink>
-//             <FooterLink href="#">Vision</FooterLink>
-//             <FooterLink href="#">Testimonials</FooterLink>
-//           </Column>
-//           <Column>
-//             <Heading>Services</Heading>
-//             <FooterLink href="#">Writing</FooterLink>
-//             <FooterLink href="#">Internships</FooterLink>
-//             <FooterLink href="#">Coding</FooterLink>
-//             <FooterLink href="#">Teaching</FooterLink>
-//           </Column> */}
-//           <Column>
-//             <Heading>Contact Us</Heading>
-//             <FooterLink href="tel:+919988040290">+91 99880 40290</FooterLink>
-//             <FooterLink href="mailto:aina.iitd@gmail.com">aina.iitd@gmail.com</FooterLink>
-//             <FooterLink href="https://www.facebook.com/aina.iitd/">Facebook</FooterLink>
-//           </Column>
-//           {/* <Column>
-//             <Heading>Social Media</Heading>
-//             <FooterLink href="#">
-//               <i className="fab fa-facebook-f">
-//                 <span style={{ marginLeft: "10px" }}>
-//                   Facebook
-//                 </span>
-//               </i>
-//             </FooterLink>
-//             <FooterLink href="#">
-//               <i className="fab fa-instagram">
-//                 <span style={{ marginLeft: "10px" }}>
-//                   Instagram
-//                 </span>
-//               </i>
-//             </FooterLink>
-//             <FooterLink href="#">
-//               <i className="fab fa-twitter">
-//                 <span style={{ marginLeft: "10px" }}>
-//                   Twitter
-//                 </span>
-//               </i>
-//             </FooterLink>
-//             <FooterLink href="#">
-//               <i className="fab fa-youtube">
-//                 <span style={{ marginLeft: "10px" }}>
-//                   Youtube
-//                 </span>
-//               </i>
-//             </FooterLink>
-//           </Column> */}
-//         </Row>
-//       </Container>
-//     </Box>
-//   );
-// };
-// export default Footer;
-
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 // import { HashLink as Link } from "react-router-hash-link";
@@ -130,17 +58,17 @@ function Footer() {
             <h4>Have a Question ?</h4>
             <ul className="list-unstyled">
               <li>
-                <span>Nipun Garg</span>{" "}
+                <span>AINA</span>{" "}
               </li>
               <li>
-                <span>99880 40290 | cs1160316@iitd.ac.in</span>{" "}
+                <span>aina.iitd@gmail.com</span>{" "}
               </li>
               <br />
               <li>
                 <span>Rahul Jain</span>{" "}
               </li>
               <li>
-                <span>87709 15084 | rahuljain13101999@gmail.com</span>{" "}
+                <span>8770915084 | rahuljain13101999@gmail.com</span>{" "}
               </li>
             </ul>
           </Col>
